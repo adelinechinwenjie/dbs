0597724f5047ac4e5b7e7126ae29392e9c292ec8
adelinechinwenjie <48095959+adelinechinwenjie@users.noreply.github.com>
2021-02-03 13:26:49 +0800

    Add files via upload

---
+++ DBS/WebCrawler/src/webcrawler/classes/CrawlerVo.java
@@ -0,0 +1,25 @@
+package webcrawler.classes;
+
+import java.util.List;
+
+public class CrawlerVo {
+	
+	public List<String> urlsList;
+	public String keyword;
+	
+	public List<String> getUrlsList() {
+		return urlsList;
+	}
+	public void setUrlsList(List<String> urlsList) {
+		this.urlsList = urlsList;
+	}
+	public String getKeyword() {
+		return keyword;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	
+	
+
+}
